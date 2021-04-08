d4bd0f588ea82807d37675e9c3d101e3fd9aabcc
Alexey Manukhin <axxapy@gmail.com>
2021-04-08 00:21:41 -0400

    hide search bar; show when search is in progress

--- extension.js
+++ extension.js
@@ -1,6 +1,10 @@
 /* exported init */
 
-const workspaceThumbnail = imports.ui.workspaceThumbnail;
+const Main = imports.ui.main
+const workspaceThumbnail = imports.ui.workspaceThumbnail
+
+//const Self = imports.misc.extensionUtils.getCurrentExtension()
+//const css = Self.imports.src
 
 class Extension {
     constructor() {
@@ -8,14 +12,39 @@ class Extension {
     }
 
     enable() {
+        this.c = {
+            searchEntry: Main.overview.searchEntry,
+            searchController: Main.overview._overview.controls._searchController,
+        }
+
         this.bkp.MAX_THUMBNAIL_SCALE = workspaceThumbnail.MAX_THUMBNAIL_SCALE
         workspaceThumbnail.MAX_THUMBNAIL_SCALE = 0.1
+
+        this.c.searchEntry.hide()
+
+        this.connectedId = this.c.searchController.connect('notify::search-active', () => {
+            if (this.c.searchController.searchActive) {
+                this.c.searchEntry.show();
+            } else {
+                this.c.searchEntry.hide();
+            }
+        })
     }
 
     disable() {
         if (this.bkp.MAX_THUMBNAIL_SCALE) {
-            workspaceThumbnail.MAX_THUMBNAIL_SCALE = this.bkp.MAX_THUMBNAIL_SCALE;
+            workspaceThumbnail.MAX_THUMBNAIL_SCALE = this.bkp.MAX_THUMBNAIL_SCALE
+            delete(this.bkp.MAX_THUMBNAIL_SCALE)
         }
+
+        if (this.connectedId) {
+            this.c.searchController.disconnect(this.connectedId)
+            delete(this.connectedId)
+        }
+
+        this.c.searchEntry.show()
+
+        delete(this.c)
     }
 }
 
