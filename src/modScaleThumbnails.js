ad9491d87673b3a86d4da94c31793f1d195d719c
Sean Bourg <sean.bourg@gmail.com>
2022-03-16 14:22:16 -0500

    Update modScaleThumbnails.js
    Modified to return if SecondaryMonitorDisplay when it is not found. Removed second check (covered by function call).

--- src/modScaleThumbnails.js
+++ src/modScaleThumbnails.js
@@ -8,21 +8,19 @@ var Mod = class extends mod.Base {
         // Thumbnails on main monitor
         this.bkp_MAX_THUMBNAIL_SCALE = workspaceThumbnail.MAX_THUMBNAIL_SCALE
         workspaceThumbnail.MAX_THUMBNAIL_SCALE = 0.1
-
-        if (SecondaryMonitorDisplay) {
-            // Thumbnails on second monitor
-            this.bkp_SecondaryMonitorDisplay_getThumbnailsHeight = SecondaryMonitorDisplay.prototype._getThumbnailsHeight
-            SecondaryMonitorDisplay.prototype._getThumbnailsHeight = function(box) {
+        
+        // Thumbnails on second monitor
+        if (!SecondaryMonitorDisplay) return;
+        this.bkp_SecondaryMonitorDisplay_getThumbnailsHeight = SecondaryMonitorDisplay.prototype._getThumbnailsHeight
+        SecondaryMonitorDisplay.prototype._getThumbnailsHeight = function(box) {
                 if (!this._thumbnails.visible)
-                    return 0;
-
+                    return 0;                
                 const [width, height] = box.get_size();
                 const { expandFraction } = this._thumbnails;
                 const [thumbnailsHeight] = this._thumbnails.get_preferred_height(width);
                 return Math.min(
                     thumbnailsHeight * expandFraction,
                     height * workspaceThumbnail.MAX_THUMBNAIL_SCALE);
-            }
         }
     }
 
@@ -31,7 +29,7 @@ var Mod = class extends mod.Base {
             workspaceThumbnail.MAX_THUMBNAIL_SCALE = this.bkp_MAX_THUMBNAIL_SCALE
         }
 
-        if (this.bkp_SecondaryMonitorDisplay_getThumbnailsHeight && SecondaryMonitorDisplay) {
+        if (this.bkp_SecondaryMonitorDisplay_getThumbnailsHeight) {
             SecondaryMonitorDisplay.prototype._getThumbnailsHeight = this.bkp_SecondaryMonitorDisplay_getThumbnailsHeight
         }
     }
