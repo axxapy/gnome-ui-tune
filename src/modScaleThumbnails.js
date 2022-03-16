4c127dbf59d136a4eec5cf723520d6e83dec1240
Sean Bourg <sean.bourg@gmail.com>
2022-03-16 17:38:47 -0500

    Removed local version changes.

--- src/modScaleThumbnails.js
+++ src/modScaleThumbnails.js
@@ -8,19 +8,20 @@ var Mod = class extends mod.Base {
         // Thumbnails on main monitor
         this.bkp_MAX_THUMBNAIL_SCALE = workspaceThumbnail.MAX_THUMBNAIL_SCALE
         workspaceThumbnail.MAX_THUMBNAIL_SCALE = 0.1
-        
+
         // Thumbnails on second monitor
         if (!SecondaryMonitorDisplay) return;
         this.bkp_SecondaryMonitorDisplay_getThumbnailsHeight = SecondaryMonitorDisplay.prototype._getThumbnailsHeight
         SecondaryMonitorDisplay.prototype._getThumbnailsHeight = function(box) {
-                if (!this._thumbnails.visible)
-                    return 0;                
-                const [width, height] = box.get_size();
-                const { expandFraction } = this._thumbnails;
-                const [thumbnailsHeight] = this._thumbnails.get_preferred_height(width);
-                return Math.min(
-                    thumbnailsHeight * expandFraction,
-                    height * workspaceThumbnail.MAX_THUMBNAIL_SCALE);
+            if (!this._thumbnails.visible)
+                return 0;
+
+            const [width, height] = box.get_size();
+            const { expandFraction } = this._thumbnails;
+            const [thumbnailsHeight] = this._thumbnails.get_preferred_height(width);
+            return Math.min(
+                thumbnailsHeight * expandFraction,
+                height * workspaceThumbnail.MAX_THUMBNAIL_SCALE);
         }
     }
 
