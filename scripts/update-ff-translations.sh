102c50612a05cfd083bc55450daf11522187eb0e
Alexey Manukhin <axxapy@gmail.com>
2023-09-11 01:03:21 -0400

    remove trailing comma

--- scripts/update-ff-translations.sh
+++ scripts/update-ff-translations.sh
@@ -17,6 +17,6 @@ done
 
 STRINGS=$(echo -e $STRINGS | sort | uniq | grep -v '"":true,')
 echo -e "/*\n * DO NOT EDIT MANUALLY\n * Generated via 'make update-ff-translations'\n */" > "$TARGET_FILE"
-echo -e "var titles = {\n$STRINGS}\n" >> "$TARGET_FILE"
+echo -e "var titles = {${STRINGS::-1}\n}\n" >> "$TARGET_FILE"
 
 echo Done
